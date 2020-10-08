3339221b632fe3b4f8058b4a68aa53fbaccfe5bb
Kzyfn <tomakata.y.0611@gmail.com>
2020-10-08 18:38:35 +0900

    ボタンテンプレート

---
+++ lib/line/line_const.rb
@@ -0,0 +1,66 @@
+class ButtonConst
+  def button_process
+    button = { "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+    "imageAspectRatio": "rectangle",
+    "imageSize": "cover",
+    "imageBackgroundColor": "#FFFFFF",
+    "title": "Menu",
+    "text": "Please select",
+    "defaultAction": {
+        "type": "uri",
+        "label": "View detail",
+        "uri": "http://example.com/page/123"
+    },
+    "actions": [
+        {
+          "type": "uri",
+          "label": "詳細を見る",
+          "uri": "https://google.com"
+        },
+        {
+          "type": "message",
+          "label": "このプロセスの提案一覧",
+          "text": "aaa"
+        },
+        {
+          "type": "uri",
+          "label": "提案を作成",
+          "uri": "https://google.com"
+        }
+    ]
+  }
+
+      button
+  end
+
+  def button_proposal
+    button = {
+    "title": "Menu",
+    "text": "Please select",
+    "defaultAction": {
+        "type": "uri",
+        "label": "View detail",
+        "uri": "https://example.com/page/123"
+    },
+    "actions": [
+        {
+          "type": "uri",
+          "label": "詳細を見る・コメントする",
+          "uri": "https://decidim-line.guild.engineer"
+        },
+        {
+          "type": "message",
+          "label": "サポートする",
+          "text": "サポートする"
+        },
+        {
+          "type": "uri",
+          "label": "エンドースする",
+          "uri": "https://google.com"
+        },
+    ]
+  }
+
+      button
+  end
+end
