468c1d06e2f72c41c8c9f2de0bc7836967552bff
Kzyfn <tomakata.y.0611@gmail.com>
2020-10-09 17:35:53 +0900

    微修正

--- lib/line/show_processes.rb
+++ lib/line/show_processes.rb
@@ -16,7 +16,7 @@ def extract_proposal_creation_permission(str)
 end  
 
 def show_processes(client, event)
-    processes = ActiveRecord::Base.connection.select_all("select * from decidim_participatory_processes where decidim_organization_id = 1;")#organization_idは適宜変更する必要あり
+    processes = Decidim::ParticipatoryProcess.where(decidim_organization_id: 1)#organization_idは適宜変更する必要あり
     caroucel = []
     hostname = 'https://3c5c61fd887a.ngrok.io/'
 
@@ -25,34 +25,33 @@ def show_processes(client, event)
 
     processes.each do |process|
        # 公開されている，かつハイライトされているものだけを表示
-      if process['published_at'] && process['promoted']
+      if process.published_at && process.promoted
           button_tmp =  button.deep_dup
 
-          button_tmp['thumbnailImageUrl'] = process["banner_image"]
-          button_tmp['title'] = extract_translation(process['title'])
-          button_tmp['text'] = extract_translation(process['short_description']).gsub(%r{</?[^>]+?>},'')
+          button_tmp['thumbnailImageUrl'] = "https://google.com"#process.banner_image
+
+          button_tmp['title'] = process.title["ja"]
+          button_tmp['text'] = process.short_description["ja"].gsub(%r{</?[^>]+?>},'')
 
 
-          button_tmp[:defaultAction][:uri] = hostname + 'processes/' + process['slug']
-          button_tmp[:actions][0][:uri] = hostname + 'processes/' + process['slug']
-          button_tmp[:actions][1][:text] = process['id'].to_s + ' ' + extract_translation(process['title']) + ' の提案一覧'
-          button_tmp[:actions][2][:uri] = hostname + 'processes/' + process['slug']
+          button_tmp[:defaultAction][:uri] = hostname + 'processes/' + process.slug
+          button_tmp[:actions][0][:uri] = hostname + 'processes/' + process.slug
+          button_tmp[:actions][1][:text] = process.id.to_s + ' ' + process.title['ja'] + ' の提案一覧'
+          button_tmp[:actions][2][:uri] = hostname + 'processes/' + process.slug
           proposals_component_id = ''
           proposal_creation_enabled = false
 
-          proposals_components = ActiveRecord::Base.connection.select_all("select * from decidim_components where participatory_space_id = #{process['id']} and manifest_name = 'proposals';")
+          proposals_components = Decidim::Component.where(participatory_space_id: process.id,  manifest_name: 'proposals')
           
           # 提案コンポーネントのURLはプロセスからはわからないので，それを見つける
 
           proposals_components.each do |proposals_component|
             proposals_component_id = ''
             proposal_creation_enabled = false
-            if proposals_component['published_at'] #一つでもパブリッシュされていれば，それで決定 (提案コンポーネントは必ず存在するとしている)
-              proposals_component_id = proposals_component['id']
+            if proposals_component.published_at #一つでもパブリッシュされていれば，それで決定 (提案コンポーネントは必ず存在するとしている)
+              proposals_component_id = proposals_component.id
 
-              if extract_proposal_creation_permission(proposals_component['settings']) == 'true'
-                proposal_creation_enabled = true
-              end  
+              proposal_creation_enabled = proposals_component.current_settings["creation_enabled"]
 
               break
             end
@@ -61,10 +60,10 @@ def show_processes(client, event)
           
           #　提案作成が可能な場合のみ提案を作成アクションを残す
           if proposal_creation_enabled then
-            button_tmp[:actions][2][:uri] = proposals_component_id != '' ? hostname + 'processes/' + process['slug'] + "/f/#{proposals_component_id}/proposals/new" : hostname + 'processes/' + process['slug']
+            button_tmp[:actions][2][:uri] = proposals_component_id != '' ? hostname + 'processes/' + process.slug + "/f/#{proposals_component_id}/proposals/new" : hostname + 'processes/' + process.slug
           else
             button_tmp[:actions][2][:label] = '現在提案を作成できません．'
-            button_tmp[:actions][2][:uri] = hostname + 'processes/' + process['slug'] + "/f/#{proposals_component_id}/proposals/"
+            button_tmp[:actions][2][:uri] = hostname + 'processes/' + process.slug + "/f/#{proposals_component_id}/proposals/"
 
           end
 
@@ -83,6 +82,8 @@ def show_processes(client, event)
                     "columns": caroucel
                 }
               }
-    client.reply_message(event['replyToken'], message)
+    result = client.reply_message(event['replyToken'], message)
+    print('result')
+    print(result.entity)
 
 end
