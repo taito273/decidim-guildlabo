6ae9329504738e0bde8fb76cf10da64c898f9bd3
Kzyfn <tomakata.y.0611@gmail.com>
2020-10-19 19:17:48 +0900

    サポートまで，投票数制限が実装できていない

--- app/services/support_proposal_service.rb
+++ app/services/support_proposal_service.rb
@@ -1,24 +1,24 @@
 require "./lib/line/line_const"
 
-class EndorseProposalService
+class SupportProposalService
 
-    def confirm_endorsement(client, event, params)
+    def confirm_support(client, event, params)
         target_proposal = Decidim::Proposals::Proposal.find(params['id'].to_i)
 
         confirmation = const_button_templates.confirmation
   
-        confirmation[:altText] = "提案 #{target_proposal.title} をいいねしますか？"
-        confirmation[:template][:text] = "提案 #{target_proposal.title} をいいねしますか？"
-        confirmation[:template][:actions][0][:data] = "id=#{params['id'].to_s}&action=endorse&flug=#{params["process_slug"]}&confirmed=true"
+        confirmation[:altText] = "提案 #{target_proposal.title} に投票しますか？"
+        confirmation[:template][:text] = "提案 #{target_proposal.title} に投票しますか？"
+        confirmation[:template][:actions][0][:data] = "id=#{params['id'].to_s}&action=support&flug=#{params["process_slug"]}&confirmed=true"
         confirmation[:template][:actions][1][:data] = "quit=true"
 
         result = client.reply_message(event['replyToken'], confirmation)
 
     end
 
-    def check_if_endorsed(decidim_uid, proposal_id)
+    def check_if_supported(decidim_uid, proposal_id)
         #ans はそのユーザーが提案にエンドースしなければ nil になる
-        ans = Decidim::Proposals::ProposalEndorsement.find_by(decidim_author_id: decidim_uid, decidim_proposal_id: proposal_id)
+        ans = Decidim::Proposals::ProposalVote.find_by(decidim_author_id: decidim_uid, decidim_proposal_id: proposal_id)
 
         if ans
             return true
@@ -27,7 +27,7 @@ class EndorseProposalService
         end
     end
 
-    def endorse_proposal(client, event, params)
+    def support_proposal(client, event, params)
 
         uid = event['source']["userId"]
         user = Decidim::Identity.find_by(uid: uid)
@@ -36,16 +36,16 @@ class EndorseProposalService
         proposal_id = params["id"].to_i
         proposal = Decidim::Proposals::Proposal.find(proposal_id)
 
-        already_endorsed = check_if_endorsed(decidim_uid, proposal_id)
+        already_endorsed = check_if_supported(decidim_uid, proposal_id)
 
         if already_endorsed
-            result = error_message(client, event, "提案 #{proposal.title} には既にいいねしています．")
+            result = error_message(client, event, "提案 #{proposal.title} には既に投票しています．")
             return
         end
 
         # ここからエンドース作業
-        new_endorsement = Decidim::Proposals::ProposalEndorsement.create!(
-            decidim_proposal_id: proposal_id.to_s, decidim_author_id: decidim_uid, decidim_author_type: Decidim::UserBaseEntity
+        new_endorsement = Decidim::Proposals::ProposalVote.create!(
+            decidim_proposal_id: proposal_id.to_s, decidim_author_id: decidim_uid
         )
 
         success_message(client, event, proposal, params['flug'])
@@ -56,14 +56,14 @@ class EndorseProposalService
 
         button = const_button_templates.result_button
 
-        button["text"] = "提案 #{proposal.title} にいいねしました．"
+        button["text"] = "提案 #{proposal.title} に投票しました．"
         # 加えてURLを指定する必要あり
         button[:defaultAction][:uri] = "#{const_button_templates.home_uri}processes/#{process_flug}/f/#{proposal.decidim_component_id}/proposals/#{proposal.id}"
         button[:actions][0][:uri] = "#{const_button_templates.home_uri}processes/#{process_flug}/f/#{proposal.decidim_component_id}/proposals/#{proposal.id}"
 
         message = {
             "type": "template",
-            "altText": "提案 #{proposal.title} にいいねしました．",
+            "altText": "提案 #{proposal.title} に投票しました．",
             "template": button
         }
         
@@ -85,5 +85,4 @@ class EndorseProposalService
     def const_button_templates
         @const_button_templates = ButtonConst.new()
     end
-    
 end
