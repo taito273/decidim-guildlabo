b5c61e9230142777ffffef54e4c3bc2f39a918ba
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-10-21 08:51:40 +0000

    提案の文字数制限etcを除去

---
+++ app/validators/etiquette_validator.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# This validator takes care of ensuring the validated content is
+# respectful, doesn't use caps, and overall is meaningful.
+class EtiquetteValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      return if value.blank?
+  
+      validate_caps(record, attribute, value)
+      validate_marks(record, attribute, value)
+      validate_long_words(record, attribute, value)
+      validate_caps_first(record, attribute, value)
+      validate_length(record, attribute, value)
+    end
+  
+    private
+  
+    def validate_caps(record, attribute, value)
+      return if value.scan(/[A-Z]/).length < value.length / 4
+  
+      record.errors.add(attribute, options[:message] || :too_much_caps)
+    end
+  
+    def validate_marks(record, attribute, value)
+      return if value.scan(/[!?¡¿]{2,}/).empty?
+  
+      record.errors.add(attribute, options[:message] || :too_many_marks)
+    end
+  
+    def validate_long_words(record, attribute, value)
+      return if value.scan(/[A-z]{35,}/).empty?
+  
+      record.errors.add(attribute, options[:message] || :long_words)
+    end
+  
+    def validate_caps_first(record, attribute, value)
+      return
+    end
+  
+    def validate_length(record, attribute, value)
+      return if  value.length > 15 || attribute != :body
+  
+      record.errors.add(attribute, options[:message] || :too_short)
+    end
+  end
